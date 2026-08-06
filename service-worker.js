86c61ad7521f95b4f3ab411190e400bb7169d81b
Marcos Morais <mvdemorais@gmail.com>
2026-08-06 17:44:54 -0300

    Copiar o codigo da sala, e lascas quando o amigo quebra

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // Carimbada pelo build a cada `npm run build`, a partir do conteúdo do jogo.
 // Fixa no código ela nunca mudava, e a cache antiga vencia para sempre: o
 // tablet ficava preso na primeira versão instalada.
-const VERSION = 'e7196d22068a'
+const VERSION = 'f939fc389d85'
 const CACHE_NAME = `mundo-blocos-${VERSION}`
 const URLS_CACHE = ['.', './index.html']
 
