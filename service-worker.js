75c0fc4438f30de2e3651872720584d08771480f
Marcos Vinicius Morais <marcosviniciusmorais@MacBook-Air-5.local>
2026-08-08 15:26:37 -0300

    Atualiza o jogo

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // Carimbada pelo build a cada `npm run build`, a partir do conteúdo do jogo.
 // Fixa no código ela nunca mudava, e a cache antiga vencia para sempre: o
 // tablet ficava preso na primeira versão instalada.
-const VERSION = 'f939fc389d85'
+const VERSION = '9e404236115d'
 const CACHE_NAME = `mundo-blocos-${VERSION}`
 const URLS_CACHE = ['.', './index.html']
 
